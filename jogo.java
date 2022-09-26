3b47128ba95cf0b2634384bf4626c5833c8cab92
Gustavo Rabelo <106771316+rabel-o@users.noreply.github.com>
2022-09-26 09:45:11 -0300

    Create jogo.java
    Nosso jogo consiste em um tabuleiro contendo uma trilha de quadrados e um conjunto de peças coloridas. No início do jogo, cada jogador recebe uma peça; todas as peças são inicialmente posicionadas logo antes da primeira casa da trilha.
    
    O jogo prossegue em rodadas. Em cada rodada, os jogadores rolam um par de dados e movem suas peças para frente um número de quadrados igual ao resultado obtido pelos dados. Os jogadores rolam os dados sempre na mesma ordem (jogador A, depois jogador B, etc.) nas rodadas. A maioria dos quadrados no tabuleiro são quadrados simples, mas alguns são “armadilhas”. Se a peça de um jogador cair em um quadrado da armadilha no final do movimento do jogador, o jogador perde a próxima rodada. Ou seja, ela/ele não jogue os dados, e sua peça fica uma rodada sem se mover.
    
    Haverá exatamente três armadilhas na trilha.
    
    O vencedor do jogo é o jogador cuja peça chega primeiro ao final da trilha. O fim da trilha é depois da última casa do tabuleiro. Considere, por exemplo, a placa da figura acima, que tem quadrados numerados de 1 a 48. No início, as peças são posicionadas no local marcado como ‘Início’, ou seja, antes do quadrado número 1. Portanto, se um jogador rolar um 7 (dados mostrando 2 e 5 por exemplo), sua peça é posicionada na casa número 7 no final da primeira rodada do jogo. Além disso, se a peça de um jogador estiver posicionada na casa 41, o jogador precisa de um resultado de rolagem de pelo menos 8 para chegar ao final da trilha e ganhar o jogo.Observe também que não haverá empate no jogo.

---
+++ jogo.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
+class Jogo{
+   
+       private int numJogadores;
+       private int numCasas;
+       private int[] posJogador;
+       private int proximoJogador;
+       private int[] armadilhas;
+       private int qtdArmadilhas = 0;
+       private boolean fimDeJogo;
+       private boolean [] podeJogar;
+    
+    public Jogo(int numJogadores, int numCasas){
+        this.numJogadores = numJogadores;
+        this.numCasas = numCasas;
+        posJogador = new int[numJogadores];
+        podeJogar = new boolean[numJogadores];
+        
+        for(int i = 0; i < numJogadores; i++){
+            posJogador[i] = 0;
+            podeJogar [i] = true;
+        }
+        proximoJogador = 0;
+        
+        armadilhas = new int[3];
+    }
+    
+    public void addArmadilha(int t){
+       if(qtdArmadilhas < 3){
+            armadilhas[qtdArmadilhas] = t;
+            qtdArmadilhas++;
+       }
+    }
+    
+    public void addMove(int d1, int d2){
+        if(fimDeJogo){
+            System.out.println("O jogo acabou");
+            return;
+        }
+        
+        if(!podeJogar[proximoJogador]){
+            System.out.printf("O jogador %d passa a vez\n", proximoJogador + 1);
+            podeJogar[proximoJogador] = true;
+            proximoJogador = (proximoJogador + 1) % numJogadores;
+        }
+        posJogador[proximoJogador] += d1 + d2;
+        System.out.printf("O jogador %d vai para a casa %d\n", proximoJogador + 1, posJogador[proximoJogador]);
+       
+        if(posJogador[proximoJogador] > numCasas){
+            fimDeJogo = true;
+            System.out.printf("O jogador %d venceu o jogo\n", proximoJogador + 1);
+            return;
+        }
+       
+        boolean c = false;
+        
+       
+        for(int i = 0; i < 3; i++){
+            if(posJogador[proximoJogador] == armadilhas[i]){
+                podeJogar[proximoJogador] = false;
+                c = true;
+                break;
+            }
+        }
+        if(c){
+            System.out.printf("O jogador %d caiu em um armadilha\n", proximoJogador + 1);
+        }
+        proximoJogador = (proximoJogador + 1) % numJogadores;
+    }
+    public String toString(){
+        String r = "";
+        for(int i = 0; i < numJogadores; i++){
+            r += String.format("PosJogador[%d] = %d\n", i+1, posJogador[i]);
+        }
+        return r;
+    }
+    
+}
+
+
+
+class Solver{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault ( Locale.US ) ;
+        Jogo jogo = new Jogo(0,0);
+
+        while(true) {
+            String line = scanner.nextLine();
+            String ui[] = line.split(" ");
+            System.out.println("$" + line);
+            if(ui[0].equals("init")) {
+                jogo = new Jogo( Integer.parseInt( ui[1]),  Integer.parseInt( ui[2]) );
+            } else if(ui[0].equals("end")) {
+                break; 
+            } else if(ui[0].equals("addArmadilha")) {
+                jogo.addArmadilha( Integer.parseInt( ui[1]) ); 
+            } else if(ui[0].equals("addMove")) {
+                int d1 = Integer.parseInt( ui[1]);
+                int d2 = Integer.parseInt( ui[2]);
+                jogo.addMove(d1, d2);
+            } else if(ui[0].equals("show")) {
+                System.out.println(jogo);
+            }else{
+                System.out.println("fail: comando invalido");
+            }
+        }
+        scanner.close();
+    }
+}
